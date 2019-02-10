069af6630d77bf167b785c68915c14509cbe6306
Nestor Vera <nestorvera@me.com>
2019-02-10 18:24:57 +0100

    Add new SocialLinks component and styles

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,33 +1,23 @@
 <template>
   <header class="sticky pin-t flex flex-col flex-no-shrink w-full md:max-w-xs h-screen p-8 text-white bg-black">
 
-    <g-link :to="{ name: 'home' }" class="block w-full rounded-full overflow-hidden">
+    <g-link :to="{ name: 'home' }" class="flex w-full rounded-full overflow-hidden">
       <h1 class="clip">{{ $static.metaData.siteName }}</h1>
       <img svg-inline svg-sprite class="block fill-current" src="../assets/img/logo.svg" :alt="$static.metaData.siteName + ' logo'" />
     </g-link>
 
     <nav class="flex flex-col mt-16 text-xl">
-      <g-link class="" :to="{ name: 'home' }">Inicio</g-link>
-      <g-link class="" :to="{ name: 'home' }">Tienda</g-link>
-      <g-link class="" :to="{ name: 'home' }">Trabajos</g-link>
-      <g-link class="" :to="{ name: 'home' }">Videos</g-link>
-      <g-link class="" :to="{ name: 'home' }">Archivo</g-link>
-      <g-link class="" :to="{ name: 'about' }">Contacto</g-link>
-    </nav>
-
-    <nav class="mt-auto">
-      <ul class="list-reset flex flex-wrap -mx-3">
-        <li class="px-3"><a class="block w-6" href="//www.instagram.com/klayaya13">
-          <img svg-inline svg-sprite class="fill-current" src="../assets/img/instagram.svg" alt="Instagram" />
-        </a></li>
-        <li class="px-3"><a class="block w-6" href="//www.twitter.com/KLaYaya">
-          <img svg-inline svg-sprite class="fill-current" src="../assets/img/twitter.svg" alt="Twitter" />
-        </a></li>
-        <li class="px-3"><a class="block w-6" href="//www.youtube.com/channel/UCIeCiOxUbuQ0nhpuf9NOWng">
-          <img svg-inline svg-sprite class="fill-current" src="../assets/img/youtube.svg" alt="YouTube" />
-        </a></li>
+      <ul class="list-reset -mx-3">
+        <li class="flex rounded overflow-hidden"><g-link class="w-full px-3 py-2 hover:bg-grey-darkest" :to="{ name: 'home' }">Inicio</g-link></li>
+        <li class="flex rounded overflow-hidden"><g-link class="w-full px-3 py-2 hover:bg-grey-darkest" :to="{ name: 'about' }">Tienda</g-link></li>
+        <li class="flex rounded overflow-hidden"><g-link class="w-full px-3 py-2 hover:bg-grey-darkest" :to="{ name: 'about' }">Trabajos</g-link></li>
+        <li class="flex rounded overflow-hidden"><g-link class="w-full px-3 py-2 hover:bg-grey-darkest" :to="{ name: 'about' }">Videos</g-link></li>
+        <li class="flex rounded overflow-hidden"><g-link class="w-full px-3 py-2 hover:bg-grey-darkest" :to="{ name: 'about' }">Archivo</g-link></li>
+        <li class="flex rounded overflow-hidden"><g-link class="w-full px-3 py-2 hover:bg-grey-darkest" :to="{ name: 'about' }">Contacto</g-link></li>
       </ul>
     </nav>
+
+    <SocialLinks class="mt-auto" />
 
   </header>
 </template>
@@ -41,7 +31,12 @@ query Home {
 </static-query>
 
 <script>
+import SocialLinks from '~/components/SocialLinks.vue'
+
 export default {
-  name: 'Header',
+  name: 'SiteHeader',
+  components: {
+    SocialLinks
+  }
 }
 </script>
---
+++ src/components/SocialLinks.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav>
+    <ul class="list-reset flex flex-wrap -m-3">
+
+      <li class="flex m-1 rounded overflow-hidden">
+        <a class="flex p-2 hover:bg-grey-darkest" href="//www.instagram.com/klayaya13">
+          <img class="w-6 h-6 fill-current" src="../assets/img/icon/instagram.svg" svg-inline svg-sprite />
+        </a>
+      </li>
+
+      <li class="flex m-1 rounded overflow-hidden">
+        <a class="flex p-2 hover:bg-grey-darkest" href="//www.twitter.com/KLaYaya">
+          <img class="w-6 h-6 fill-current" src="../assets/img/icon/twitter.svg" svg-inline svg-sprite />
+        </a>
+      </li>
+
+      <li class="flex m-1 rounded overflow-hidden">
+        <a class="flex p-2 hover:bg-grey-darkest" href="//www.youtube.com/channel/UCIeCiOxUbuQ0nhpuf9NOWng">
+          <img class="w-6 h-6 fill-current" src="../assets/img/icon/youtube.svg" svg-inline svg-sprite />
+        </a>
+      </li>
+
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'SocialLinks',
+}
+</script>

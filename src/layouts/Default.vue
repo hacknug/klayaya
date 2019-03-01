ac6e4e86c626753a0c5280c63c4389d44cd68ebb
Nestor Vera <nestorvera@me.com>
2019-03-01 12:05:31 +0100

    Fix body bg-color not matching the app's

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -2,7 +2,7 @@
   <fragment>
 
     <SiteHeader />
-    <main :class="classArray"><slot /></main>
+    <main class="flex flex-col w-full px-4 py-8"><slot /></main>
 
   </fragment>
 </template>
@@ -12,14 +12,17 @@ import SiteHeader from '~/components/SiteHeader.vue'
 
 export default {
   components: { SiteHeader },
-  computed: {
-    classArray() {
-      return [
-        'flex flex-col w-full px-4 py-8',
-        this.$store.state.activeTheme === 'light'
-          ? 'bg-grey-lightest'
-          : 'bg-grey-darkest',
-      ]
+  metaInfo() {
+    return {
+      bodyAttrs: {
+        class: [
+          'md:flex min-h-screen',
+          this.$store.state.activeTheme === 'light'
+            ? 'text-black bg-grey-lightest'
+            : 'text-white bg-grey-darkest',
+          'text-base leading-normal font-sans antialiased'
+        ].join(' '),
+      },
     }
   },
 }

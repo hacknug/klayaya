9b0442e9394ecc4d8fe205c4a82a2e1dc3156014
Nestor Vera <nestorvera@me.com>
2019-03-05 14:09:58 +0100

    Fix Netlify Form missing form-name input

--- src/components/BaseForm.vue
+++ src/components/BaseForm.vue
@@ -1,5 +1,8 @@
 <template>
-  <form :name="name" method="POST" data-netlify="true"><slot /></form>
+  <form :name="name" method="POST" data-netlify="true">
+    <input type="hidden" name="form-name" :value="name" />
+    <slot />
+  </form>
 </template>
 
 <script>
